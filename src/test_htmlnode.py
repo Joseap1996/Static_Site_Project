389d77c0b48d70c5958657c40e138f592f87ecd9
Joseap1996 <joseap1996@live.com>
2025-04-30 20:59:00 -0500

    F: Added LeafNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -22,3 +22,21 @@ class TestHTMLNode(unittest.TestCase):
         # Test with empty properties dictionary
         node = HTMLNode(tag="div", value="", children=None, props={})
         self.assertEqual(node.props_to_html(), "")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>',
+        )
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+        
+if __name__ == "__main__":
+    unittest.main()
