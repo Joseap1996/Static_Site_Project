6a60937067f0017ec67d1e431318138920ffd0b3
Joseap1996 <joseap1996@live.com>
2025-05-18 15:16:05 -0500

    K: added split_nodes_image/link functions

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -21,6 +21,68 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        images = extract_markdown_images(old_node.text)
+        if not images:
+            new_nodes.append(old_node)
+            continue
+        current_text = old_node.text
+        for image_alt, image_url in images:
+            image_markdown = f"![{image_alt}]({image_url})"
+            parts = current_text.split(image_markdown, 1)
+
+            if parts[0]:
+                split_nodes.append(TextNode(parts[0], TextType.TEXT))
+            
+            split_nodes.append(TextNode(image_alt, TextType.IMAGE, image_url))
+
+            if len(parts) > 1:
+                current_text = parts[1]
+            else:
+                current_text = ""
+
+        if current_text:
+            split_nodes.append(TextNode(current_text, TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+        
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        links = extract_markdown_links(old_node.text)
+        if not links:
+            new_nodes.append(old_node)
+            continue
+        current_text = old_node.text
+        for link_text, link_url in links:
+            link_markdown = f"[{link_text}]({link_url})"
+            parts = current_text.split(link_markdown, 1)
+            if parts[0]:
+                split_nodes.append(TextNode(parts[0], TextType.TEXT))
+            
+            split_nodes.append(TextNode(link_text, TextType.LINK, link_url))
+
+            if len(parts) > 1:
+                current_text = parts[1]
+            else:
+                current_text = ""
+
+        if current_text:
+            split_nodes.append(TextNode(current_text, TextType.TEXT))
+        new_nodes.extend(split_nodes)
+    return new_nodes
+
+
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
